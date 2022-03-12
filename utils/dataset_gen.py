36434abd88f60effc742e3792961bd5d3e8aeb45
ecolon08 <ernestocolon08@gmail.com>
2022-03-12 13:14:08 -0500

    unet-2d training pipeline initial commit

--- utils/dataset_gen.py
+++ utils/dataset_gen.py
@@ -57,7 +57,7 @@ def rf_xgb_ds_generator(samples, coh_flag=False, int_flag=False):
             if coh_flag:
                 # pre-event coherence
                 with rasterio.open(pth[2]) as src:
-                    #coh_pre_event = np.transpose(np.squeeze(src.read()), axes=(1, 2, 0))
+                    # coh_pre_event = np.transpose(np.squeeze(src.read()), axes=(1, 2, 0))
                     coh_pre_event = np.transpose(src.read(), axes=(1, 2, 0))
 
                     if np.any(np.isnan(coh_pre_event)):
@@ -65,7 +65,7 @@ def rf_xgb_ds_generator(samples, coh_flag=False, int_flag=False):
 
                 # co-event coherence
                 with rasterio.open(pth[3]) as src:
-                    #coh_co_event = np.transpose(np.squeeze(src.read()), axes=(1, 2, 0))
+                    # coh_co_event = np.transpose(np.squeeze(src.read()), axes=(1, 2, 0))
                     coh_co_event = np.transpose(src.read(), axes=(1, 2, 0))
 
                     if np.any(np.isnan(coh_co_event)):
@@ -90,7 +90,7 @@ def rf_xgb_ds_generator(samples, coh_flag=False, int_flag=False):
 
         Y_train.append(lbl)
 
-    #Stack the lists into np arrays now
+    # Stack the lists into np arrays now
 
     X_train = np.stack(X_train)
     Y_train = np.stack(Y_train)
@@ -110,6 +110,40 @@ def rf_xgb_ds_generator(samples, coh_flag=False, int_flag=False):
 ####################################
 
 
+def unet_load_ds_df(train_df_pth, val_df_pth, test_df_pth):
+    # recall_dir = r"C:\Users\ernes\OneDrive - The Cooper Union for the Advancement of Science and Art\Thesis-DESKTOP-UID4081\flood_mapping\10m_resolution"
+
+    # Create lists to hold the sample paths
+    test_samples = []
+    train_samples = []
+    val_samples = []
+
+    # Load the csv files with the train, validation, and test splits
+    train_fn_df = pd.read_csv(train_df_pth)
+    val_fn_df = pd.read_csv(val_df_pth)
+    test_fn_df = pd.read_csv(test_df_pth)
+
+    # Loop through the CSV files and extract + organize the different chips
+    for idx, row in test_fn_df.iterrows():
+        test_samples.append((row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['s2_lbl']))
+
+    for idx, row in train_fn_df.iterrows():
+        train_samples.append(
+            (row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['s2_lbl']))
+
+    for idx, row in val_fn_df.iterrows():
+        val_samples.append((row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['s2_lbl']))
+
+    # concatenate the train and validation dataframes
+    train_val_fn_df = pd.concat([train_fn_df, val_fn_df])
+
+    # grab the dataset sizes
+    train_size = len(train_samples)
+    val_size = len(val_samples)
+    test_size = len(test_samples)
+
+    return train_val_fn_df, test_fn_df, train_size, val_size, test_size
+
 
 def create_samples_list(params):
     """
@@ -128,71 +162,74 @@ def create_samples_list(params):
 
     if scenario == 'co_event_coh_only':
         test_samples = []
-        train_val_samples =[]
+        train_val_samples = []
 
         for idx, row in test_df.iterrows():
-            #test_samples.append((row['co_event_coh'], row['s1_lbl']))
+            # test_samples.append((row['co_event_coh'], row['s1_lbl']))
             test_samples.append((row['co_event_coh'], row['s2_lbl']))
-            #test_samples.append((row['pre_event_coh'], row['s1_lbl']))
+            # test_samples.append((row['pre_event_coh'], row['s1_lbl']))
 
         for idx, row in train_val_df.iterrows():
-            #train_val_samples.append((row['co_event_coh'], row['s1_lbl']))
+            # train_val_samples.append((row['co_event_coh'], row['s1_lbl']))
             train_val_samples.append((row['co_event_coh'], row['s2_lbl']))
-            #train_val_samples.append((row['pre_event_coh'], row['s1_lbl']))
+            # train_val_samples.append((row['pre_event_coh'], row['s1_lbl']))
 
     elif scenario == 'co_event_intensity_only':
         test_samples = []
-        train_val_samples =[]
+        train_val_samples = []
 
         for idx, row in test_df.iterrows():
-            #test_samples.append((row['s1'], row['s1_lbl']))
+            # test_samples.append((row['s1'], row['s1_lbl']))
             test_samples.append((row['s1'], row['s2_lbl']))
 
         for idx, row in train_val_df.iterrows():
-            #train_val_samples.append((row['s1'], row['s1_lbl']))
+            # train_val_samples.append((row['s1'], row['s1_lbl']))
             train_val_samples.append((row['s1'], row['s2_lbl']))
 
     elif scenario == 'pre_co_event_coh':
         test_samples = []
-        train_val_samples =[]
+        train_val_samples = []
 
         for idx, row in test_df.iterrows():
-            #test_samples.append((row['pre_event_coh'], row['co_event_coh'], row['s1_lbl']))
+            # test_samples.append((row['pre_event_coh'], row['co_event_coh'], row['s1_lbl']))
             test_samples.append((row['pre_event_coh'], row['co_event_coh'], row['s2_lbl']))
 
         for idx, row in train_val_df.iterrows():
-            #train_val_samples.append((row['pre_event_coh'], row['co_event_coh'], row['s1_lbl']))
+            # train_val_samples.append((row['pre_event_coh'], row['co_event_coh'], row['s1_lbl']))
             train_val_samples.append((row['pre_event_coh'], row['co_event_coh'], row['s2_lbl']))
 
     elif scenario == 'pre_co_event_intensity':
         test_samples = []
-        train_val_samples =[]
+        train_val_samples = []
 
         for idx, row in test_df.iterrows():
-            #test_samples.append((row['s1'], row['pre_event_grd'], row['s1_lbl']))
+            # test_samples.append((row['s1'], row['pre_event_grd'], row['s1_lbl']))
             test_samples.append((row['s1'], row['pre_event_grd'], row['s2_lbl']))
 
         for idx, row in train_val_df.iterrows():
-            #train_val_samples.append((row['s1'], row['pre_event_grd'], row['s1_lbl']))
+            # train_val_samples.append((row['s1'], row['pre_event_grd'], row['s1_lbl']))
             train_val_samples.append((row['s1'], row['pre_event_grd'], row['s2_lbl']))
 
     elif scenario == 'pre_co_event_int_coh':
         test_samples = []
-        train_val_samples =[]
+        train_val_samples = []
 
         for idx, row in test_df.iterrows():
-            #test_samples.append((row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['s1_lbl']))
-            test_samples.append((row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['s2_lbl']))
+            # test_samples.append((row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['s1_lbl']))
+            test_samples.append(
+                (row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['s2_lbl']))
 
         for idx, row in train_val_df.iterrows():
-            #train_val_samples.append((row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['s1_lbl']))
-            train_val_samples.append((row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['s2_lbl']))
+            # train_val_samples.append((row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['s1_lbl']))
+            train_val_samples.append(
+                (row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['s2_lbl']))
 
     return train_val_samples, test_samples
 
 
 IMG_SIZE = 512
 class_weight = {0: 1, 1: 1}
+
 
 def read_sample(data_path: str) -> tuple:
     """
@@ -209,8 +246,8 @@ def read_sample(data_path: str) -> tuple:
     # the target path is always the last item
     target_path = path[-1].decode('utf-8')
 
-    #image_path, target_path = path[0].decode('utf-8'), path[1].decode('utf-8')
-    #image_path, pre_event_coh_path, co_event_coh_path, target_path = path[0].decode('utf-8'), path[1].decode('utf-8'), path[2].decode('utf-8'), path[3].decode('utf-8')
+    # image_path, target_path = path[0].decode('utf-8'), path[1].decode('utf-8')
+    # image_path, pre_event_coh_path, co_event_coh_path, target_path = path[0].decode('utf-8'), path[1].decode('utf-8'), path[2].decode('utf-8'), path[3].decode('utf-8')
 
     # loop through the rest of the paths and append to list
     pth_lst = list()
@@ -222,10 +259,10 @@ def read_sample(data_path: str) -> tuple:
         tgt = np.transpose(src.read(), axes=(1, 2, 0)).astype(np.int8)
 
         # translate the labels so they are all non-negative. I am getting NaN's when training the model
-        #tgt = tgt + np.ones(tgt.shape)
+        # tgt = tgt + np.ones(tgt.shape)
 
         # pluck not-valid pixels mask
-        #sample_weights = np.where(tgt == 0, 0, 1)
+        # sample_weights = np.where(tgt == 0, 0, 1)
         data_mask = np.where(tgt == -1, 0, 1)
         sample_weights = np.where(data_mask == 0, class_weight[0], class_weight[1])
 
@@ -233,7 +270,7 @@ def read_sample(data_path: str) -> tuple:
         tgt = np.where(tgt == -1, 0, tgt)
 
         # Flip the labels to check something real quick
-        #tgt = np.where(tgt == 0, 1, 0)
+        # tgt = np.where(tgt == 0, 1, 0)
 
     # Loop through the rest of the image stack, transpose and then stack
     stack = list()
@@ -251,26 +288,26 @@ def read_sample(data_path: str) -> tuple:
                     stack.append(np.squeeze(raster[:, :, band]))
             else:
                 # squeeze and append to list
-                    stack.append(np.squeeze(raster[:, :, 0]))
+                stack.append(np.squeeze(raster[:, :, 0]))
 
-    #with rasterio.open(image_path) as src:
+    # with rasterio.open(image_path) as src:
     #    s1_img = np.transpose(src.read(), axes=(1, 2, 0))
 
-    #with rasterio.open(pre_event_coh_path) as src:
+    # with rasterio.open(pre_event_coh_path) as src:
     #    pre_event_coh = np.transpose(src.read(), axes=(1, 2, 0))
 
-    #with rasterio.open(co_event_coh_path) as src:
+    # with rasterio.open(co_event_coh_path) as src:
     #    co_event_coh = np.transpose(src.read(), axes=(1, 2, 0))
 
     # with the data mask
-    #img = np.stack((np.squeeze(s1_img[:, :, 0]), np.squeeze(s1_img[:, :, 1]), np.squeeze(pre_event_coh[:, :, 0]), np.squeeze(co_event_coh[:, :, 0]), np.squeeze(data_mask)))
+    # img = np.stack((np.squeeze(s1_img[:, :, 0]), np.squeeze(s1_img[:, :, 1]), np.squeeze(pre_event_coh[:, :, 0]), np.squeeze(co_event_coh[:, :, 0]), np.squeeze(data_mask)))
 
     # Optimization attempt: Add the normalized coherence difference
-    #norm_coh_diff = (stack[-2] - stack[-1]) / ((stack[-2] + stack[-1] + 1e10))
-    #stack.append(norm_coh_diff)
+    # norm_coh_diff = (stack[-2] - stack[-1]) / ((stack[-2] + stack[-1] + 1e10))
+    # stack.append(norm_coh_diff)
 
     # without the data mask
-    #img = np.stack((np.squeeze(s1_img[:, :, 0]), np.squeeze(s1_img[:, :, 1]), np.squeeze(pre_event_coh[:, :, 0]), np.squeeze(co_event_coh[:, :, 0])))
+    # img = np.stack((np.squeeze(s1_img[:, :, 0]), np.squeeze(s1_img[:, :, 1]), np.squeeze(pre_event_coh[:, :, 0]), np.squeeze(co_event_coh[:, :, 0])))
     img = np.stack(stack)
     img = np.transpose(img, axes=(1, 2, 0))
 
@@ -280,12 +317,12 @@ def read_sample(data_path: str) -> tuple:
 @tf.function
 def tf_read_sample(data_path: str) -> dict:
     # wrap custom dataloadeer into tensorflow
-    #[image, target] = tf.py_function(read_sample, [data_path], [tf.uint16, tf.uint8])
+    # [image, target] = tf.py_function(read_sample, [data_path], [tf.uint16, tf.uint8])
 
     [image, target, sample_weight] = tf.py_function(read_sample, [data_path], [tf.float32, tf.int8, tf.uint8])
 
     # explicitly set tensor shapes
-    #image.set_shape((256, 256, 10))
+    # image.set_shape((256, 256, 10))
     num_bands = image.shape[-1]
 
     image.set_shape((IMG_SIZE, IMG_SIZE, num_bands))
@@ -294,13 +331,13 @@ def tf_read_sample(data_path: str) -> dict:
 
     return {'image': image, 'target': target, 'sample_weight': sample_weight}
 
+
 @tf.function
 def load_sample(sample: dict) -> tuple:
     # convert to tf image
     image = tf.image.resize(sample['image'], (IMG_SIZE, IMG_SIZE))
     target = tf.image.resize(sample['target'], (IMG_SIZE, IMG_SIZE))
     sample_weight = tf.image.resize(sample['sample_weight'], (IMG_SIZE, IMG_SIZE))
-
 
     # flip image at random to augment
     if tf.random.uniform(()) > 0.5:
@@ -311,30 +348,31 @@ def load_sample(sample: dict) -> tuple:
     # Standardize
 
     # Unstack the image to pluck the sample_weights for standardization
-    #image_wo_samp_weights = image[:, :, 0:-1]
-    #mask = image[:, :, -1]
+    # image_wo_samp_weights = image[:, :, 0:-1]
+    # mask = image[:, :, -1]
 
-    #mean = tf.math.reduce_mean(image_wo_samp_weights, axis=(0, 1))
-    #std = tf.math.reduce_std(image_wo_samp_weights, axis=(0, 1))
-    #image_std = (image_wo_samp_weights - mean) / (std + 1e-10)
+    # mean = tf.math.reduce_mean(image_wo_samp_weights, axis=(0, 1))
+    # std = tf.math.reduce_std(image_wo_samp_weights, axis=(0, 1))
+    # image_std = (image_wo_samp_weights - mean) / (std + 1e-10)
 
     # skip standardization to test coherence only models
-    #mean = tf.math.reduce_mean(image, axis=(0, 1))
-    #std = tf.math.reduce_std(image, axis=(0, 1))
-    #image_std = (image - mean) / (std + 1e-10)
-    #image = image_std
+    # mean = tf.math.reduce_mean(image, axis=(0, 1))
+    # std = tf.math.reduce_std(image, axis=(0, 1))
+    # image_std = (image - mean) / (std + 1e-10)
+    # image = image_std
 
-    #image = tf.experimental.numpy.dstack((image_std, mask))
+    # image = tf.experimental.numpy.dstack((image_std, mask))
     # I can also normalize between [0, 1] but I found faster convergence by standardizing
-    #min = tf.math.reduce_min(image, axis=(0, 1))
-    #max = tf.math.reduce_max(image, axis=(0, 1))
+    # min = tf.math.reduce_min(image, axis=(0, 1))
+    # max = tf.math.reduce_max(image, axis=(0, 1))
 
-    #image = (image - min) / ( (max - min) + 1e-10)
+    # image = (image - min) / ( (max - min) + 1e-10)
 
-    #image = (image - tf.math.reduce_min(image, axis=(0, 1))) / (tf.math.reduce_max(image, axis=(0, 1)) - tf.math.reduce_min(image, axis=(0, 1)))
+    # image = (image - tf.math.reduce_min(image, axis=(0, 1))) / (tf.math.reduce_max(image, axis=(0, 1)) - tf.math.reduce_min(image, axis=(0, 1)))
     target = tf.cast(target, tf.uint8)
 
     return image, target, sample_weight
+
 
 @tf.function
 def load_test_sample(sample: dict) -> tuple:
@@ -344,23 +382,21 @@ def load_test_sample(sample: dict) -> tuple:
     sample_weight = tf.image.resize(sample['sample_weight'], (IMG_SIZE, IMG_SIZE))
 
     # grab image and targets
-    #img = tf.unstack(image, axis=-1)
-    #tgt = tf.constant(tgt)
+    # img = tf.unstack(image, axis=-1)
+    # tgt = tf.constant(tgt)
 
     target = tf.cast(target, tf.uint8)
 
     # standardize the test images
-    #mean = tf.math.reduce_mean(image, axis=(0, 1))
-    #std = tf.math.reduce_std(image, axis=(0, 1))
-    #image_std = (image - mean) / (std + 1e-10)
-    #image = image_std
+    # mean = tf.math.reduce_mean(image, axis=(0, 1))
+    # std = tf.math.reduce_std(image, axis=(0, 1))
+    # image_std = (image - mean) / (std + 1e-10)
+    # image = image_std
 
     return image, target, sample_weight
 
 
-
-def ds_creation(params):
-
+def unet_ds_creation(params):
     train_val_samples = params['train_val_list']
     test_samples = params['test_list']
 
@@ -390,7 +426,6 @@ def ds_creation(params):
     test_dataset = test_ds.cache()
 
     return train_dataset, val_dataset, test_dataset
-
 
 
 def create_samples_list_hand_lbl(params):
@@ -449,7 +484,8 @@ def create_samples_list_hand_lbl(params):
 
         for idx, row in test_df.iterrows():
             try:
-                test_samples.append((row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['hand_lbl']))
+                test_samples.append(
+                    (row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['hand_lbl']))
             except:
                 test_samples.append(
                     (row['s1'], row['pre_event_grd'], row['pre_event_coh'], row['co_event_coh'], row['s2_lbl']))
@@ -458,7 +494,6 @@ def create_samples_list_hand_lbl(params):
 
 
 def ds_creation_hand_lbl(params):
-
     test_samples = params['test_list']
 
     # create tensorflow dataset from file links
@@ -473,4 +508,3 @@ def ds_creation_hand_lbl(params):
     test_dataset = test_ds.cache()
 
     return test_dataset
-
