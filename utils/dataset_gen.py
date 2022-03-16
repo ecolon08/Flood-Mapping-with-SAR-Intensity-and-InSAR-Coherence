a5cf85545fb3f3b90986ad43f9d4a0f8c94844f8
ecolon08 <ernestocolon08@gmail.com>
2022-03-15 22:15:45 -0400

    data set generator refactor

--- utils/dataset_gen.py
+++ utils/dataset_gen.py
@@ -17,11 +17,18 @@ import pandas as pd
 
 
 ####################################
-# XGBoost Pipeline
+#      XGBoost Pipeline
 ####################################
 
 def xgboost_load_ds_samples(train_df_pth, val_df_pth, test_df_pth):
+    """
+    Function to process training, validation, and test input image paths for the XGBoost models
 
+    :param train_df_pth: dataframe with training data set input file paths
+    :param val_df_pth: dataframe with validation data set input file paths
+    :param test_df_pth: dataframe with test data set input file paths
+    :return: lists for train, val, and test data file paths along with corresponding data set sizes
+    """
     # Create lists to hold the sample paths
     test_samples = []
     train_samples = []
@@ -53,9 +60,18 @@ def xgboost_load_ds_samples(train_df_pth, val_df_pth, test_df_pth):
 
 def rf_xgb_ds_generator(samples, coh_flag=False, int_flag=False):
     """
-    :param samples:
-    :param coh_flag:
-    :return:
+    Function to generate an XGBoost or Random Forest pixel-wise classification data set for training
+    The function generates the data set depending on the input scenario requested. The coherence and intensity
+    booleans determine the scenario.
+
+    Co-event intensity: coh_flag = False, int_flag = True
+    Bi-temporal intensity: coh_flag = False, int_flag = True
+    Bi-temporal intensity and coherence: coh_flag = True, int_flag = True
+
+    :param samples: list containing tuples with input file paths
+    :param coh_flag: coherence band boolean flag
+    :param int_flag: intensity band boolean flag
+    :return: X_train, Y_train as ndarrays with training data set, number of samples and number of features
     """
 
     X_train = list()
@@ -146,8 +162,14 @@ def rf_xgb_ds_generator(samples, coh_flag=False, int_flag=False):
 
 
 def unet_load_ds_df(train_df_pth, val_df_pth, test_df_pth):
-    # recall_dir = r"C:\Users\ernes\OneDrive - The Cooper Union for the Advancement of Science and Art\Thesis-DESKTOP-UID4081\flood_mapping\10m_resolution"
+    """
+    Function to process training, validation, and test input image paths for the U-Net models
 
+    :param train_df_pth: dataframe with training data set input file paths
+    :param val_df_pth: dataframe with validation data set input file paths
+    :param test_df_pth: dataframe with test data set input file paths
+    :return: dataframes for train, val, and test data file paths along with corresponding data set sizes
+    """
     # Create lists to hold the sample paths
     test_samples = []
     train_samples = []
@@ -184,6 +206,7 @@ def create_samples_list(params):
     """
     Function that ingests the train + val and test dataframes containing the filepaths and returns a list of tuples with
     the filepaths. The list of tuples returned is assembled based on the particular multi-temporal scenario.
+
     :param train_val_df: dataframe with the train + val sample filepaths
     :param test_df: dataframe with the test sample filepaths
     :param scenario: string indicating multi-temporal scenario
@@ -265,12 +288,11 @@ def create_samples_list(params):
 IMG_SIZE = 512
 class_weight = {0: 1, 1: 1}
 
-
 def read_sample(data_path: str) -> tuple:
     """
-    Function to read the data samples when including the coherence maps
-    :param data_path:
-    :return:
+    Function to read the data path and return a tuple with input image, ground truth label, and pixel weights
+    :param data_path: string with input file paths
+    :return: tuple with input image, ground truth label, and pixel weights
     """
 
     path = data_path.numpy()
@@ -281,9 +303,6 @@ def read_sample(data_path: str) -> tuple:
     # the target path is always the last item
     target_path = path[-1].decode('utf-8')
 
-    # image_path, target_path = path[0].decode('utf-8'), path[1].decode('utf-8')
-    # image_path, pre_event_coh_path, co_event_coh_path, target_path = path[0].decode('utf-8'), path[1].decode('utf-8'), path[2].decode('utf-8'), path[3].decode('utf-8')
-
     # loop through the rest of the paths and append to list
     pth_lst = list()
     for pth in path[:-1]:
@@ -293,19 +312,11 @@ def read_sample(data_path: str) -> tuple:
     with rasterio.open(target_path) as src:
         tgt = np.transpose(src.read(), axes=(1, 2, 0)).astype(np.int8)
 
-        # translate the labels so they are all non-negative. I am getting NaN's when training the model
-        # tgt = tgt + np.ones(tgt.shape)
-
-        # pluck not-valid pixels mask
-        # sample_weights = np.where(tgt == 0, 0, 1)
         data_mask = np.where(tgt == -1, 0, 1)
         sample_weights = np.where(data_mask == 0, class_weight[0], class_weight[1])
 
         # Map the no valid pixels to not-water
         tgt = np.where(tgt == -1, 0, tgt)
-
-        # Flip the labels to check something real quick
-        # tgt = np.where(tgt == 0, 1, 0)
 
     # Loop through the rest of the image stack, transpose and then stack
     stack = list()
@@ -325,24 +336,6 @@ def read_sample(data_path: str) -> tuple:
                 # squeeze and append to list
                 stack.append(np.squeeze(raster[:, :, 0]))
 
-    # with rasterio.open(image_path) as src:
-    #    s1_img = np.transpose(src.read(), axes=(1, 2, 0))
-
-    # with rasterio.open(pre_event_coh_path) as src:
-    #    pre_event_coh = np.transpose(src.read(), axes=(1, 2, 0))
-
-    # with rasterio.open(co_event_coh_path) as src:
-    #    co_event_coh = np.transpose(src.read(), axes=(1, 2, 0))
-
-    # with the data mask
-    # img = np.stack((np.squeeze(s1_img[:, :, 0]), np.squeeze(s1_img[:, :, 1]), np.squeeze(pre_event_coh[:, :, 0]), np.squeeze(co_event_coh[:, :, 0]), np.squeeze(data_mask)))
-
-    # Optimization attempt: Add the normalized coherence difference
-    # norm_coh_diff = (stack[-2] - stack[-1]) / ((stack[-2] + stack[-1] + 1e10))
-    # stack.append(norm_coh_diff)
-
-    # without the data mask
-    # img = np.stack((np.squeeze(s1_img[:, :, 0]), np.squeeze(s1_img[:, :, 1]), np.squeeze(pre_event_coh[:, :, 0]), np.squeeze(co_event_coh[:, :, 0])))
     img = np.stack(stack)
     img = np.transpose(img, axes=(1, 2, 0))
 
@@ -351,13 +344,13 @@ def read_sample(data_path: str) -> tuple:
 
 @tf.function
 def tf_read_sample(data_path: str) -> dict:
-    # wrap custom dataloadeer into tensorflow
-    # [image, target] = tf.py_function(read_sample, [data_path], [tf.uint16, tf.uint8])
+    """
+    wrap custom dataloadeer into tensorflow
+    """
 
     [image, target, sample_weight] = tf.py_function(read_sample, [data_path], [tf.float32, tf.int8, tf.uint8])
 
     # explicitly set tensor shapes
-    # image.set_shape((256, 256, 10))
     num_bands = image.shape[-1]
 
     image.set_shape((IMG_SIZE, IMG_SIZE, num_bands))
@@ -369,6 +362,9 @@ def tf_read_sample(data_path: str) -> dict:
 
 @tf.function
 def load_sample(sample: dict) -> tuple:
+    """
+    Function to load samples using tensorflow data pipeline
+    """
     # convert to tf image
     image = tf.image.resize(sample['image'], (IMG_SIZE, IMG_SIZE))
     target = tf.image.resize(sample['target'], (IMG_SIZE, IMG_SIZE))
@@ -380,30 +376,6 @@ def load_sample(sample: dict) -> tuple:
         image = tf.image.rot90(tf.image.flip_left_right(image), k=num_rot)
         target = tf.image.rot90(tf.image.flip_left_right(target), k=num_rot)
 
-    # Standardize
-
-    # Unstack the image to pluck the sample_weights for standardization
-    # image_wo_samp_weights = image[:, :, 0:-1]
-    # mask = image[:, :, -1]
-
-    # mean = tf.math.reduce_mean(image_wo_samp_weights, axis=(0, 1))
-    # std = tf.math.reduce_std(image_wo_samp_weights, axis=(0, 1))
-    # image_std = (image_wo_samp_weights - mean) / (std + 1e-10)
-
-    # skip standardization to test coherence only models
-    # mean = tf.math.reduce_mean(image, axis=(0, 1))
-    # std = tf.math.reduce_std(image, axis=(0, 1))
-    # image_std = (image - mean) / (std + 1e-10)
-    # image = image_std
-
-    # image = tf.experimental.numpy.dstack((image_std, mask))
-    # I can also normalize between [0, 1] but I found faster convergence by standardizing
-    # min = tf.math.reduce_min(image, axis=(0, 1))
-    # max = tf.math.reduce_max(image, axis=(0, 1))
-
-    # image = (image - min) / ( (max - min) + 1e-10)
-
-    # image = (image - tf.math.reduce_min(image, axis=(0, 1))) / (tf.math.reduce_max(image, axis=(0, 1)) - tf.math.reduce_min(image, axis=(0, 1)))
     target = tf.cast(target, tf.uint8)
 
     return image, target, sample_weight
@@ -416,22 +388,17 @@ def load_test_sample(sample: dict) -> tuple:
     target = tf.image.resize(sample['target'], (IMG_SIZE, IMG_SIZE))
     sample_weight = tf.image.resize(sample['sample_weight'], (IMG_SIZE, IMG_SIZE))
 
-    # grab image and targets
-    # img = tf.unstack(image, axis=-1)
-    # tgt = tf.constant(tgt)
-
     target = tf.cast(target, tf.uint8)
-
-    # standardize the test images
-    # mean = tf.math.reduce_mean(image, axis=(0, 1))
-    # std = tf.math.reduce_std(image, axis=(0, 1))
-    # image_std = (image - mean) / (std + 1e-10)
-    # image = image_std
 
     return image, target, sample_weight
 
 
 def unet_ds_creation(params):
+    """
+    Function to generate a tensorflow data set generator given train, validation, and test file paths as lists
+    :param params: dictionary with train, val, and test file path lists
+    :return: tensorflow data set generators for train, validation, and test
+    """
     train_val_samples = params['train_val_list']
     test_samples = params['test_list']
 
@@ -529,6 +496,11 @@ def create_samples_list_hand_lbl(params):
 
 
 def ds_creation_hand_lbl(params):
+    """
+    Function to create tensorflow data set generator for the hand-labeled data set
+    :param params: dictionary with test data set list including input file paths
+    :return: tensorflow data set generator for hand-labeled data set
+    """
     test_samples = params['test_list']
 
     # create tensorflow dataset from file links
